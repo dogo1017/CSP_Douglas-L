38a3cd60129977833a08b605b08f75955b5c3073
dogo1017 <douglas.london@ucas-edu.net>
2025-01-31 12:35:58 -0700

    finished practice.c

--- Variables/practice.c
+++ Variables/practice.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-char *Name[] = {"Douglas"};
+char *Name = {"Douglas"};
 int number_between_1_and_10 = 6;
 int number_between_100_and_1000 = 936;
-char breakfast = "nothing";
-char fav_color = "cyan";
-char school_name = "Utah Academy of Science";
+char *breakfast = "nothing";
+char *fav_color = "cyan";
+char *school_name = "Utah Academy of Science";
 int year = 2025;
-char eye_color = "blue";
+char *eye_color = "blue";
 int age = 14;
-char favorite_school_subject = "CSP";
+char *favorite_school_subject = "CSP";
 
 int main(void){
-    printf("My name is %s. A number between 1 ad 10 is %d, while a number between 1 and 1000 is %d. I ate %s for breakfast and my favorite color is %s. I go to %s for school and my favorite subject is %s. It is year %d, and i am %s years old as of today, and My eye color is %s.", Name, number_between_1_and_10, number_between_100_and_1000, breakfast, fav_color, school_name, favorite_school_subject, year, age, eye_color);
+    printf("My name is %s. A number between 1 and 10 is %d, while a number between 1 and 1000 is %d. I ate %s for breakfast and my favorite color is %s. I go to %s for school and my favorite subject is %s. It is year %d, I am %d years old as of today, and My eye color is %s.", Name, number_between_1_and_10, number_between_100_and_1000, breakfast, fav_color, school_name, favorite_school_subject, year, age, eye_color);
     return 0;
 }
