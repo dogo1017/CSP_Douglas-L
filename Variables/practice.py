24b358125c66c2ad8eee9913e62b1dd67dad8269
dogo1017 <douglas.london@ucas-edu.net>
2025-01-31 14:41:36 -0700

    Create practice.py

---
+++ Variables/practice.py
@@ -0,0 +1,14 @@
+#python variables practice
+
+name = "Douglas"
+number_between_1_and_10 = 9
+number_between_100_and_1000 = 999
+breakfast = "nothing"
+fav_color = "cyan"
+school_name = "Utah Academy of Sciences"
+year = 2025
+eye_color = "blue"
+your_age = 14
+Fav_school_subject = "CSP"
+
+print("My name is", name".)
