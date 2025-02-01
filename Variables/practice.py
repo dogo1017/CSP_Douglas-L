328cea2022e31f063c7b11a708d1a611482b5300
dogo1017 <douglas.london@ucas-edu.net>
2025-01-31 20:05:53 -0700

    Finshed python variable practice

--- Variables/practice.py
+++ Variables/practice.py
@@ -11,4 +11,4 @@ eye_color = "blue"
 your_age = 14
 Fav_school_subject = "CSP"
 
-print("My name is", name".)
+print("my name is", name, ". A number between 1 and 10 is", number_between_1_and_10, ", While a number between 100 and 1000 is", number_between_100_and_1000, ". I ate", breakfast, "for breakfast today and my favorite color is", fav_color, ". I am", your_age, "as of", year, "and I got to", school_name, "My eyecolor is", eye_color, "and my favorite school subject is", Fav_school_subject, "because I like programming.")
