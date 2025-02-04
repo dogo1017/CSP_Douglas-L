37e562532599e14a533c3ab213e857623593e2ed
dogo1017 <douglas.london@ucas-edu.net>
2025-02-04 11:49:11 -0700

    fixed stuff

---
+++ expressions/expression_practce.py
@@ -0,0 +1,12 @@
+# Douglas London, expressions practice
+
+first_equation = 7-24/8*4+6
+second_equation = 1
+third_equation = 1
+fourth_equation = 1
+sixth_equation = 1
+seventh_equation = 1
+eighth_equation = 1
+nineth_equation = 1
+
+print(first_equation)
