99721e9eb7e8a0d2277c730e27f79807f01d3f23
dogo1017 <douglas.london@ucas-edu.net>
2025-02-05 19:46:19 +0000

    worked on pow

--- expressions/practice.C
+++ expressions/practice.C
@@ -8,8 +8,8 @@ float fourth_equation = (17-6/2)+4*3;
 float fifth_equation = -2*(1*4-2/2)+(6+2-3);
 float sixth_equation = -1*((3-4*7)/5)-2*24/6;
 float seventh_equation = (3*pow(5,2)/15)-(5-pow(2,2));
-float eighth_equation = (1**4*2**2+3**3)-2**5/4;
-float nineth_equation = (22/2-2*5)**2+(4-6/6)**2;
+float eighth_equation = (pow(1, 4)*pow(2,2)+pow(3,3))-pow(2,5)/4;
+float nineth_equation = (22/2-2*5)**2+pow((4-6/6),2);
 
 printf("%f"first_equation);
 
