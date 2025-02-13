e871de27751e373589578ad099c29475b606924c
dogo1017 <douglas.london@ucas-edu.net>
2025-02-13 11:48:38 -0700

    finished silly scentences C

---
+++ strings/silly_scentences.c
@@ -0,0 +1,84 @@
+// Douglas London, Silly scentences C
+#include <stdio.h>
+// empty string variables for user words (minimum 3)
+
+char adj1[50];
+char noun2;
+char verb_pt3; 
+char adv4;
+char adj5;
+char noun6;
+char noun7;
+char adj8;
+char verb9;
+char adv10; 
+char verb_pt11; 
+char adj12;
+
+int main(void){
+    
+//welcome user
+
+
+printf("welcome to my silly scentences program. When the program asks for a specific category of word you will think of one and respond with one of those words to fill out a scentence.\n");
+
+//ask for words (print with scanf) (in C we need to tell the user they can only type one word)
+
+//print the story with inserted variables ("welcome %s to my progtram", name)
+
+printf("adjective:\n");
+scanf("%s", adj1);
+printf("noun:\n");
+scanf("%s", noun2);
+printf("verb in past tense:\n");
+scanf("%s", verb_pt3);
+printf("adverb:\n");
+scanf("%s", adv4);
+printf("adjective:\n");
+scanf("%s", adj5);
+printf("noun:\n");
+scanf("%s", noun6);
+printf("noun:\n");
+scanf("%s", noun7);
+printf("adjective:\n");
+scanf("%s", adj8);
+printf("verb:\n");
+scanf("%s", verb9);
+printf("adverb:\n");
+scanf("%s", adv10);
+printf("verb in past tense:\n");
+scanf("%s", verb_pt11);
+printf("adjective:\n");
+scanf("%s", adj12);
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+//Today I went to the zoo. I saw a(n)
+//___________(adjective)
+//_____________(Noun) jumping up and down in its tree.
+//He _____________(verb, past tense) __________(adverb)
+//through the large tunnel that led to its _______(adjective)
+//__________(noun). I got some peanuts and passed
+//them through the cage to a gigantic gray _______(noun)
+//towering above my head. Feeding that animal made
+//me hungry. I went to get a __________(adjective) scoop
+//of ice cream. It filled my stomach. Afterwards I had to
+//__________(verb) __________ (adverb) to catch our bus.
+//When I got home I __________(verb, past tense) my
+//mom for a __________(adjective) day at the zoo. 
