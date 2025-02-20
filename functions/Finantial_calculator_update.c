457963e5f189aa39c06068e4f68aff4c5fcce7b1
dogo1017 <douglas.london@ucas-edu.net>
2025-02-20 11:49:22 -0700

    finished python calculator update

---
+++ functions/Finantial_calculator_update.c
@@ -0,0 +1,32 @@
+// Douglas London, Finantial calculator update C
+#include <stdio.h>
+
+float inputs(char type[20], int input[50]){
+    printf("What is your monthly %s cost:\n", type);
+    scanf("%f", &input);
+    return;
+}
+
+float income;
+float utilities;
+float groceries;
+float transportation;
+
+
+int main(void){
+    
+printf("Hello, this is my finantial calculator that calculates savings, spendins, and other finantial expenses.\n");
+printf("What is your monthly income:\n");
+scanf("%f", income);
+
+float rent = inputs("rent");
+
+
+
+
+
+
+
+
+    return 0;
+}
--- functions/Finantial_calculator_update.py
+++ functions/Finantial_calculator_update.py
@@ -4,15 +4,28 @@ print("Hello, this is my finantial calculator that calculates savings, spendins,
 
 def collecting_data(type):
     return input(f"What is your monthly {type} cost:\n")
-    
+ 
+def info(cost, income, type):
+    percent = cost/income*100
+    print(f"Your {type} is ${cost:.2f}, which is {percent}% of your income.")
+
+
     
 income = float(input("What is your monthly income:\n"))
-rent = collecting_data("rent")
-utilities = collecting_data("utilities")
-groceries = collecting_data("groceries")
-transportation = collecting_data("transportation")
+rent = float(collecting_data("rent"))
+utilities = float(collecting_data("utilities"))
+groceries = float(collecting_data("groceries"))
+transportation = float(collecting_data("transportation"))
 
 savings = income*0.1
+spendings = income-savings-rent-utilities-groceries-transportation
+
+info(rent, income, "rent")
+info(utilities, income, "utillities")
+info(rent, income, "groceries")
+info(rent, income, "transportation")
+info(savings, income, "savings")
+info(spendings, income, "spendings")
 
 
 
