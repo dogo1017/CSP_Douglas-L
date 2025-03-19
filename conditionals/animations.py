ce3fcbf2dacead82b498fd493a8764699bca382b
dogo1017 <douglas.london@ucas-edu.net>
2025-03-19 17:48:11 +0000

    animation

--- conditionals/animations.py
+++ conditionals/animations.py
@@ -3,11 +3,11 @@
 import time
 import sys
 
-animation = "123456789_-_-_-_-_-_-|_|"
+animation = "13456789_-_-_-_-_-_-|_|"
 start_time = time.time()
 while True:
     for i in range(24):
-        time.sleep(0.2)  # Feel free to experiment with the speed here
+        time.sleep(0.9)  # Feel free to experiment with the speed here
         sys.stdout.write("\r" + animation[i % len(animation)])
         sys.stdout.flush()
     if time.time() - start_time > 100:  # The animation will last for 10 seconds
