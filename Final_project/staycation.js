845a7aab26ae4563df0d7978b7a095cb945c5660
dogo1017 <douglas.london@ucas-edu.net>
2025-05-01 13:23:44 -0600

    tried to fix arrow

--- Final_project/staycation.js
+++ Final_project/staycation.js
@@ -1,8 +1,12 @@
 function hidden() {
     const arrow = document.getElementById("arrow");
+    const hiddenImages = document.getElementById("hidden-images");
+
     if (arrow.innerHTML === "►") {
         arrow.innerHTML = "▼";
+        hiddenImages.style.display = "block";
     } else {
-        arrow.innerHTML = "►"; 
+        arrow.innerHTML = "►";
+        hiddenImages.style.display = "none";
     }
 }
